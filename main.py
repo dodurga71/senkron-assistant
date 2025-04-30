5a31bbf9c081ec692a2e1335829171c63692e055
dodurga71 <78793327+dodurga71@users.noreply.github.com>
2025-04-30 20:37:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.routes.message_router import router as message_router
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "SENKRON AI Assistant is running."}
+
+app.include_router(message_router)
